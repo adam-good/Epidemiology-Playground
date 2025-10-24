1520bdaef3b53e5d409a860a2d32cf0f34a3e3ab
Adam Good <dev.adamgood@gmail.com>
2025-10-24 11:16:41 -0400

    reworked disease and illness

--- sir.py
+++ sir.py
@@ -27,29 +27,34 @@ class Disease:
         name {string} -- The name of the disease (default: {None})
     """
 
-    def __init__(self, infection_rate: float, mortality_rate: float,
+    def __init__(self, infection_rate: float, case_fatality_rate: float,
                  incubation_period: float, illness_period: float,
                  name: str | None = None):
         self._infection_rate: float = infection_rate
-        self._mortality_rate: float = mortality_rate
+        self._cfr: float = case_fatality_rate
         self._incubation_period: float = incubation_period
         self._illness_period:  float = illness_period
         self.name: str | None = name
 
-    def get_incubation_period(self) -> float:
-        """The Disease's Average Incubation Period        
-        Returns:
-            float -- An average incubation period
-        """
-        return self._incubation_period
+        self._incubation_period_dist: Distribution = PoissonDist(lam=self._incubation_period)
+        self._illness_period_dist: Distribution = PoissonDist(lam=self._illness_period)
 
-    def get_illness_period(self) -> float:
-        """Sample's the illness period distribution
-        
+    def get_incubation_period(self) -> int:
+        """Sample's an incubation period from a statistical distribution
+        Returns:
+            int -- An average incubation period
+        """
+        return self._incubation_period_dist.sample()
+
+    def get_illness_period(self) -> int:
+        """Sample's an illness period from a statistical distribution        
         Returns:
             float -- An average illness period
         """
-        return self._illness_period
+        return self._illness_period_dist.sample()
+
+    def get_infection_rate(self) -> float:
+        return self._infection_rate
 
 class Infection:
     """Couples disease to person when person gets sick and tracks disease progression
@@ -62,29 +67,31 @@ class Infection:
         FINISHED = 2
 
     def __init__(self, person, disease):
-        self.person: Person = person
-        self.disease: Disease = disease
-        self.incubation_period: int = PoissonDist(self.disease.get_incubation_period()).sample()
-        self.illness_period: int = PoissonDist(self.disease.get_illness_period()).sample()
-
+        self._person: Person = person
+        self._disease: Disease = disease
+        self._incubation_period: int = self._disease.get_incubation_period()
+        self._illness_period: int = self._disease.get_illness_period()
+        
         # Solving for event success probability given cumulative probability of death over illness time (using geometric distribution)
         # https://math.stackexchange.com/questions/2161184/solving-for-the-cdf-of-the-geometric-probability-distribution
-        self.daily_mortality: float = 1 - np.exp( np.log(1 - self.disease.mortality_rate) / self.illness_period )
+        self.daily_mortality: float = 1 - np.exp( np.log(1 - self._disease._cfr) / self._illness_period )
 
         self.status: self.InfectionState = Infection.InfectionState.INCUBATION
+        self._incubation_counter: int = 0
+        self._illness_counter: int = 0
 
     def advance_infection(self):
         """Advance the progress of the infection
         """
         if self.status == Infection.InfectionState.INCUBATION:
-            self.incubation_period -= 1
-            if self.incubation_period <= 0:
-                self.status = Infection.InfectionState.ILLNESS
+            self._increment_incubation_counter()
+            if self._incubation_counter >= self._incubation_period:
+                self._set_status_illness()
 
         elif self.status == Infection.InfectionState.ILLNESS:
-            self.illness_period -= 1
-            if self.illness_period <= 0:
-                self.status = Infection.InfectionState.FINISHED
+            self._increment_illness_counter()
+            if self._illness_counter >= self._illness_period:
+                self._set_status_finished()
 
     def get_recovery_time(self):
         """Calculate Remaining Time Until Infection is Over (disregarding death)
@@ -93,6 +100,21 @@ class Infection:
             int -- The time until the infection will change to the FINISHED state
         """
         return self.incubation_period + self.illness_period
+
+    def _increment_incubation_counter(self):
+        self._incubation_counter += 1
+
+    def _increment_illness_counter(self):
+        self._illness_counter += 1
+
+    # NOTE: This is to avoid some type hinting issues
+    def _set_status_incubation(self):
+        self.status: self.InfectionState = Infection.InfectionState.INCUBATION
+    def _set_status_illness(self):
+        self.status: self.InfectionState = Infection.InfectionState.ILLNESS
+    def _set_status_finished(self):
+        self.InfectionState: self.InfectionState = Infection.InfectionState.FINISHED
+    
 
 # TODO: Again, probability distributions can be abstracted
 class Person:
@@ -130,7 +152,7 @@ class Person:
         """
         if self.is_infected() and person.is_susceptible():
             r: float = np.random.uniform()
-            if r < self.disease.infection_rate:
+            if r < self.disease.get_infection_rate():
                 person.infect(self.disease)
 
     def infect(self, disease: Disease):
